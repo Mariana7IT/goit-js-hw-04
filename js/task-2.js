879eccf596430d663e55d673b2991db7f72958fe
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-04-06 22:26:20 +0300

    task-3

--- js/task-2.js
+++ js/task-2.js
@@ -3,12 +3,17 @@
 //  Функція очікує один параметр: days — масив об’єктів.Кожен об’єкт описує день тижня та кількість калорій calories, спожитих спортсменом, у цей день.
 //  Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.У консоль будуть виведені результати її викликів.
 function calcAverageCalories(days) {
-    if (days.lenght === 0) {
+    if (days.length === 0) {
         return 0;
     }
+    let allCalories = 0;
+    for (let i = 0; i < days.length; i++){
+        allCalories += days[i].calories;
+    }
+
+    const caloriesAverage = allCalories / days.length;
+    return Math.round(caloriesAverage)
 }
-
-
 
 console.log(calcAverageCalories([
     { day: "monday", calories: 3010 },
